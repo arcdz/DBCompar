f0a8625410fad17fbaf85a1c5edb84195ec2041b
GitHub Action <action@github.com>
2025-04-17 06:00:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/arcdz/dbcompar/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arcdz/dbcompar/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.arcdz.dbcompar.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arcdz.dbcompar.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
